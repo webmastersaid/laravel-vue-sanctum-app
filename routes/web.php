a27f84a5c0cd67e231dae71d48b31aebe3778566
Said <46088969+webmastersaid@users.noreply.github.com>
2023-11-04 20:34:20 +0500

    ui auth

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +16,7 @@ use App\Http\Controllers\IndexController;
 */
 
 Route::get('/', IndexController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
