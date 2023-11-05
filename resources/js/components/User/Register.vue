4456b5b433f0ed7292e5e1261b7a6907c2d77320
Said <46088969+webmastersaid@users.noreply.github.com>
2023-11-05 17:43:34 +0500

    registration

--- resources/js/components/User/Register.vue
+++ resources/js/components/User/Register.vue
@@ -1,9 +1,59 @@
 <script>
 export default {
+    data() {
+        return {
+            user: {
+                name: null,
+                email: null,
+                password: null,
+                password_confirmation: null,
+            },
+            message: null,
+        }
+    },
+    methods: {
+        register() {
+            axios.get('/sanctum/csrf-cookie')
+                .then(response => {
+                    axios.post('/register', this.user)
+                        .then(res => {
+                            this.$router.push({ name: 'user.index' })
+                        })
+                        .catch(e => {
+                            this.message = e.message
+                        })
+                })
+        }
+    }
 }
 </script>
 <template>
     <div>
         <h1>Register page</h1>
+        <div class="mb-3">
+            <label for="name" class="form-label">Name</label>
+            <input v-model="user.name" type="text" id="name" placeholder="Bob" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input v-model="user.email" type="email" id="email" placeholder="name@localhost" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input v-model="user.password" type="password" id="password" placeholder="password" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="password_confirmation" class="form-label">Confirm password</label>
+            <input v-model="user.password_confirmation" type="password" id="password_confirmation"
+                placeholder="confirm password" class="form-control">
+        </div>
+        <div v-if="message" class="mb-3">
+            <div class="alert alert-danger" role="alert">
+                {{ message }}
+            </div>
+        </div>
+        <div class="mb-3">
+            <input @click.prevent="register" type="submit" class="btn btn-primary" value="Login">
+        </div>
     </div>
 </template>
