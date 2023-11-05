d859cd56cd63dff636afbdadb820513adab7f347
Said <46088969+webmastersaid@users.noreply.github.com>
2023-11-05 17:39:08 +0500

    interceptors & credentials

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -7,9 +7,19 @@ import 'bootstrap';
  */
 
 import axios from 'axios';
+import router from './router';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.withCredentials = true;
+window.axios.interceptors.response.use({}, e => {
+    if (e.response.status === 401 || e.response.status === 419) {
+        const token = localStorage.getItem('x_xsrf_token')
+        if (token)
+            localStorage.removeItem('x_xsrf_token')
+        router.push({ name: 'user.login' })
+    }
+})
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
