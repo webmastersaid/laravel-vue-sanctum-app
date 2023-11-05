f29e22483ecc567a5b461309776d375ead9c6f90
Said <46088969+webmastersaid@users.noreply.github.com>
2023-11-05 17:42:59 +0500

    user view

--- resources/js/components/User/Index.vue
+++ resources/js/components/User/Index.vue
@@ -1,9 +1,31 @@
 <script>
 export default {
+    data() {
+        return {
+            user: {
+                id: null,
+                name: null,
+                email: null,
+            }
+        }
+    },
+    methods: {
+        getUser() {
+            axios.get('/api/user')
+                .then(res => {
+                    this.user = res.data.data
+                })
+        }
+    },
+    mounted() {
+        this.getUser()
+    }
 }
 </script>
 <template>
     <div>
-        <h1>Get page</h1>
+        <h1>{{user.name}}</h1>
+        <small># {{ user.id }}</small>
+        <div>Email: {{ user.email }}</div>
     </div>
 </template>
