e41d81ced465b7456a6473fd97925172fb7c1c22
Said <46088969+webmastersaid@users.noreply.github.com>
2023-11-05 17:42:41 +0500

    logout & token check

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,15 +1,39 @@
 <script>
 export default {
+    data() {
+        return {
+            token: null,
+        }
+    },
+    methods: {
+        logout() {
+            axios.post('/logout')
+                .then(res => {
+                    localStorage.removeItem('x_xsrf_token')
+                    this.$router.push({ name: 'user.login' })
+                })
+        },
+        getToken() {
+            this.token = localStorage.getItem('x_xsrf_token')
+        }
+    },
+    mounted() {
+        this.getToken()
+    },
+    updated() {
+        this.getToken()
+    }
 }
 </script>
 <template>
     <div class="container">
         <nav class="nav">
             <RouterLink class="nav-link" :to="{ name: 'index' }">Index</RouterLink>
-            <RouterLink class="nav-link" :to="{ name: 'user.index' }">User</RouterLink>
-            <RouterLink class="nav-link" :to="{ name: 'user.login' }">Login</RouterLink>
-            <RouterLink class="nav-link" :to="{ name: 'user.register' }">Register</RouterLink>
+            <RouterLink v-if="token" class="nav-link" :to="{ name: 'user.index' }">User</RouterLink>
+            <RouterLink v-if="!token" class="nav-link" :to="{ name: 'user.login' }">Login</RouterLink>
+            <RouterLink v-if="!token" class="nav-link" :to="{ name: 'user.register' }">Register</RouterLink>
+            <a v-if="token" @click.prevent="logout" href="#" class="nav-link">Logout</a>
         </nav>
-        <RouterView></RouterView>
+        <RouterView :key="$route.fullPath"></RouterView>
     </div>
 </template>
